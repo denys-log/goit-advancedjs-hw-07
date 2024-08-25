eb75337877bbb2ef9274889f6e2e99c618036110
Денис Уцеха <d.utsekha@reactor.com.ua>
2024-08-25 17:13:29 +0300

    add tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,56 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person has entered the house.");
+    } else {
+      console.log("Door is closed. Cannot enter.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door is now open.");
+    } else {
+      console.log("Incorrect key. Door remains closed.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +59,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
